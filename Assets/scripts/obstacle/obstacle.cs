c8eb0de7dfb8ed534900839017853029fbad4e0f
HuyGia <huygia2077@users.noreply.github.com>
2026-07-11 11:54:19 +0700

    change road texture and improve bullet impact effect

--- Assets/scripts/obstacle/obstacle.cs
+++ Assets/scripts/obstacle/obstacle.cs
@@ -9,45 +9,68 @@ public class obstacle : MonoBehaviour
     [SerializeField] private Vector2 ashesSize;
     [SerializeField] private short particleCounts;
 
+    [SerializeField] private ParticleSystem bulletFragments;
+    [SerializeField] private Color bulletFragmentColor;
     private void playEffect()
     {   
-        // Get modules such as main, shape, emission from the particle prefab
-        var main = ashesEffect.main; 
-        var shape  = ashesEffect.shape;
-        var emission = ashesEffect.emission;
-
-        // Get the area (size of obstacle)
-        float area = boxCollider.size.x * boxCollider.size.y;
-
-        // Modify particle's start size
-        main.startSize = new ParticleSystem.MinMaxCurve(ashesSize.x, ashesSize.y);
-
-        // Modify particle's shape
-        shape.radius = Mathf.Max(boxCollider.size.x, boxCollider.size.y) * 0.12f;
-
-        // Modify particle's number of particles
-        emission.SetBursts(new ParticleSystem.Burst[]
+        if (ashesEffect != null)
         {
-            new ParticleSystem.Burst(0f, particleCounts, particleCounts)
-        });
+            // Get modules such as main, shape, emission from the particle prefab
+            var main = ashesEffect.main; 
+            var shape  = ashesEffect.shape;
+            var emission = ashesEffect.emission;
 
-        Instantiate(ashesEffect, transform.position, Quaternion.identity);
+            // Get the area (size of obstacle)
+            float area = boxCollider.size.x * boxCollider.size.y;
+
+            // Modify particle's start size
+            main.startSize = new ParticleSystem.MinMaxCurve(ashesSize.x, ashesSize.y);
+
+            // Modify particle's shape
+            shape.radius = Mathf.Max(boxCollider.size.x, boxCollider.size.y) * 0.12f;
+
+            // Modify particle's number of particles
+            emission.SetBursts(new ParticleSystem.Burst[]
+            {
+                new ParticleSystem.Burst(0f, particleCounts, particleCounts)
+            });
+
+            Instantiate(ashesEffect, transform.position, Quaternion.identity);
+        }
     }
 
 
     void Start()
     {
-        playEffect();
+        if (this.CompareTag("map_obstacle") == false)
+        {
+            playEffect();
+        }
     }
     
     public void removeObject()
     {
         // Rescan the map before destroy an obstacle
+        playEffect();
         Bounds bound = boxCollider.bounds;
         boxCollider.enabled = false;
         AstarPath.active.UpdateGraphs(bound);
-        playEffect();
         Destroy(gameObject);
+    }
+
+
+    void OnCollisionEnter2D(Collision2D other)
+    {
+        if (other.gameObject.CompareTag("bullet"))
+        {
+            if (bulletFragments != null)
+            {
+                ParticleSystem fragment = Instantiate(bulletFragments, other.gameObject.GetComponent<Transform>().position, Quaternion.identity);
+                var main = fragment.main;
+                main.startColor = new ParticleSystem.MinMaxGradient(bulletFragmentColor);
+            }
+            Destroy(other.gameObject);
+        }
     }
 
 }
